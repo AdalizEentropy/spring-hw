cede9e9d9eaf3f6b528ffb9ac77070606c80d818
Волохова Светлана Викторовна <adalizaentropy@yandex.ru>
2023-07-12 21:39:27 +0300

    fix: fix after review

--- webflux/src/main/java/ru/nspk/webflux/model/User.java
+++ webflux/src/main/java/ru/nspk/webflux/model/User.java
@@ -3,6 +3,7 @@ package ru.nspk.webflux.model;
 import lombok.Getter;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceCreator;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.relational.core.mapping.MappedCollection;
 import org.springframework.data.relational.core.mapping.Table;
@@ -14,23 +15,28 @@ import java.util.*;
 @Table("users")
 @Getter
 public class User implements UserDetails {
-    @Id private String username;
+    @Id private final String username;
     private final String password;
     private final boolean enabled;
 
     @MappedCollection(idColumn = "username")
     @Transient
-    private Set<Authority> roles;
+    private final List<Authority> authorities;
 
-    public User(String username, String password, boolean enabled) {
+    public User(String username, String password, boolean enabled, List<Authority> authorities) {
         this.username = username;
         this.password = password;
         this.enabled = enabled;
-        this.roles = new HashSet<>();
+        this.authorities = authorities;
+    }
+
+    @PersistenceCreator
+    public User(String username, String password, boolean enabled) {
+        this(username, password, enabled, new ArrayList<>());
     }
 
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return roles;
+        return authorities;
     }
 
     @Override
--- webflux/src/main/java/ru/nspk/webflux/service/SecUserDetailsService.java
+++ webflux/src/main/java/ru/nspk/webflux/service/SecUserDetailsService.java
@@ -6,13 +6,14 @@ import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import ru.nspk.webflux.model.Authority;
 import ru.nspk.webflux.model.User;
 import ru.nspk.webflux.repository.AuthorityReactiveRepository;
 import ru.nspk.webflux.repository.UserReactiveRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -22,8 +23,16 @@ public class SecUserDetailsService implements ReactiveUserDetailsService {
 
     @Override
     public Mono<UserDetails> findByUsername(String username) {
-        Mono<User> user = userReactiveRepository.findByUsername(username);
-        Flux<Authority> authorities = authorityReactiveRepository.findByUsername(username);
-        return user.cast(UserDetails.class);
+        Mono<List<Authority>> authoritiesMono =
+                authorityReactiveRepository.findByUsername(username).collectList();
+        Mono<User> userMono = userReactiveRepository.findByUsername(username);
+        return userMono.zipWith(
+                authoritiesMono,
+                (user1, authorities) ->
+                        new User(
+                                user1.getUsername(),
+                                user1.getPassword(),
+                                user1.isEnabled(),
+                                authorities));
     }
 }
